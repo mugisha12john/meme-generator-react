64aa9745e9f104d69bb7629dbf675b4be1fc67d2
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-03-18 17:09:28 +0200

    feat: put an image in form

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
 import img from "../assets/hero.png";
 export default function Main() {
+  const [meme, setMeme] = useState({
+    topText: "One time i will eat earth",
+    bottomText: "one day i will carry earth",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+  function handleChange(e) {
+    const { value, name } = e.currentTarget;
+    setMeme((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
   return (
     <>
       <main>
@@ -10,6 +23,7 @@ export default function Main() {
               type="text"
               placeholder="One does not simply"
               name="topText"
+              onChange={handleChange}
             />
           </label>
 
@@ -19,14 +33,15 @@ export default function Main() {
               type="text"
               placeholder="Walk into Mordor"
               name="bottomText"
+              onChange={handleChange}
             />
           </label>
           <button>Get a new meme image 🖼</button>
         </div>
         <div className="meme">
-          <img src={img} />
-          <span className="top">welcome to this</span>
-          <span className="bottom">tiger is waste</span>
+          <img src={meme.imageUrl} />
+          <span className="top">{meme.topText}</span>
+          <span className="bottom">{meme.bottomText}</span>
         </div>
       </main>
     </>
